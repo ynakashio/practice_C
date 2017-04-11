67beed11b266fc831aa10799888e6415a42ab4a2
Yuko Nakashio <kuppylemonade.0d.t@gmail.com>
2017-04-11 18:54:35 +0900

    r011

--- r011.c
+++ r011.c
@@ -1,11 +1,30 @@
-#include<studio.h>
+#include<stdio.h>
 
-main()
+int main()
 {
 	int kokugo,shakai,rika;
-	int goukei,heikin;
+	int goukei;
+    float average;
+   
+    /* raw_inputする変数がintの時は、&でアドレスを明示する
+       */
 
-	
+    printf("国語の点数=");
+	scanf("%d",&kokugo);
 
+    printf("\n社会の点数=");
+    scanf("%d",&shakai);
+
+    printf("\n理科の点数=");
+    scanf("%d",&rika);
+
+    goukei = kokugo + shakai + rika;
+    printf("\n合計は%d点",goukei);
+
+    
+    average = (float)goukei/3;
+    printf("\n平均は%f",average);
+
+    return 0;
 
 }
