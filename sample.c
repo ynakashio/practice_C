b7ca4cdf88d69d796e05d0f59a79e1c4590543bf
Yuko Nakashio <kuppylemonade.0d.t@gmail.com>
2017-04-11 18:25:15 +0900

    sample

--- sample.c
+++ sample.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 /*
-コメント
+int %d
+float &f
+char %c
 */
 
 int main(void){
-    printf("hello world!\n");
+    int x = 10;
+    float y = 5.2;
+    char c = 'A';
+    x += 5;
+    printf("%d\n",x*x);
+    printf("x = %d, y=%f, c =%c\n", x,y,c);
     return 0;
 }
